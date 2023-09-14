3746324060999cf4e60124b2032d23ecc8c31ccf
Abdi <abdi.abdulle@outlook.dk>
2023-09-14 21:50:48 +0200

    views and templates added, app and project level url router provided

--- mkgif/models.py
+++ mkgif/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Animation(models.Model):
+      name = models.CharField(max_length=200)
+
+
+class Image(models.Model):
+      def image_path(self, filename):
+          return f'{self.animation.pk}/{filename}'
+
+      animation = models.ForeignKey('Animation', on_delete=models.CASCADE)
+      image = models.ImageField(upload_to=image_path)
--- mkgif/views.py
+++ mkgif/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from .models import Animation, Image
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+       anim = Animation.objects.create(name=request.POST['name'])
+       for img in request.FILES.getlist('imgs'):
+           Image.objects.create(animation=anim, image=img)
+
+    anims = Animation.objects.all()
+    context = {
+            'anims': anims
+            }
+    return render(request, 'mkgif/index.html', context)
+
+def details(request, pk):
+    anim = get_object_or_404(Animation, pk=pk)
+    images = Image.objects.filter(animation=pk)
+    context = {
+            'anim': anim,
+            'images': images
+            }
+    return render(request, 'mkgif/details.html', context)
+
